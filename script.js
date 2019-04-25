0e38631b953177450a75a17150e319f70b14b511
phuong0 <44624226+phuong0@users.noreply.github.com>
2019-04-25 16:05:44 -0700

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,28 @@
+var deposit;
+var name;
+var depositorName = [];
+var moneyAdd = [];
+var total = 0;
+
+$(".newDeposit").click(function() {
+    deposit = $(".accountDeposit").val();
+    name = $(".accountName").val();
+    if (deposit === "" || name === ""){
+        $(".error").text("You did not input all of the nesscessay information.");
+    } else {
+        $(".error").text(" ");
+        depositorName.push(name);
+        moneyAdd.push(parseInt(deposit));
+        $(".numberT").text(depositorName.length);
+        moneyAdd.forEach(function(money){
+             total += money;
+             $(".totalA").text(total);
+        });
+        $(".ledger").append("<p>" + name + "</p><p class='ledgerBorder'>$" + deposit + "</p>");
+        $(".accountDeposit").val("");
+        $(".accountName").val("");
+    }
+});
+$(".update").click(function() {
+    alert("Your later transaction was $" + moneyAdd[moneyAdd.length - 1] + " by " + depositorName[depositorName.length - 1] );
+});
